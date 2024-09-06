6d19c7660c9021cf3cd64ba7fd42c5e71f1fc5fa
Paavo Hietanen <hietanen.paavo@gmail.com>
2024-09-06 12:02:19 +0300

    StellarObject graphic is randomized

--- stellar_objects.py
+++ stellar_objects.py
@@ -1,11 +1,19 @@
 from PyQt5.QtCore import QRectF
 from PyQt5.QtGui import QImage
 from PyQt5.QtWidgets import QGraphicsItem
+import random
+
+stellar_obj_graphics = [
+    'C:\\Users\\35840\\Documents\\Personal\\Personal code\\ikarus\\graphics\\ruined_planet.png',
+    'C:\\Users\\35840\\Documents\\Personal\\Personal code\\ikarus\\graphics\\planet_red.png',
+    'C:\\Users\\35840\\Documents\\Personal\\Personal code\\ikarus\\graphics\\asteroid.png',
+    'C:\\Users\\35840\\Documents\\Personal\\Personal code\\ikarus\\graphics\\planet_blue.png'
+]
 
 class StellarObject(QGraphicsItem):
     def __init__(self):
         QGraphicsItem.__init__(self)
-        self.icon = 'C:\\Users\\35840\\Documents\\Personal\\Personal code\\ikarus\\graphics\\ruined_planet.png'
+        self.icon = random.choice(stellar_obj_graphics)
         self.coords = [100, 80]
         self.width = 200
         self.height = 200
